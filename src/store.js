b8ef5355aa5ebb4cd167b59e9a793fc242ef2fc3
David Konsumer <konsumer@jetboystudio.com>
2017-08-28 10:56:37 -0700

    better state persistance, improved defaults

--- src/store.js
+++ src/store.js
@@ -1,58 +1,64 @@
 /* global localStorage */
-import { createStore } from 'redux'
+import { compose, createStore } from 'redux'
+import { persistStore, autoRehydrate } from 'redux-persist'
 
 import mets from './mets.json'
 
-const categories = Object.values(mets)
-  .map(m => m.category)
-  .filter((v, i, cats) => cats.indexOf(v) === i)
-
-const weight = localStorage.weight && parseFloat(localStorage.weight)
-
 const defaultState = {
-  mets,
-  categories,
+  mets: {},
+  categories: [],
   showAddDialog: false,
   showSettingsDialog: false,
-  log: localStorage.log ? JSON.parse(localStorage.log) : [],
+  log: [],
   category: null,
   action: null,
   currentTime: Date.now(),
-  weight,
-  setWeight: weight || ''
+  weight: undefined,
+  setWeight: localStorage['reduxPersist:weight'] ? localStorage['reduxPersist:weight'] : '180'
 }
 
+const composeEnhancers =
+  typeof window === 'object' &&
+  process && process.env && process.env.NODE_ENV === 'development' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose
+
 const store = createStore(
-  (state = defaultState, action) => {
+  (state = {}, action) => {
+    const log = state.log ? state.log.slice() : []
     if (action.type === 'set') return {...state, ...action.payload}
     if (action.type === 'add_log') {
-      const log = state.log.slice()
       log.push({start: Date.now(), running: true, activity: state.activity})
-      localStorage.log = JSON.stringify(log)
       return {...state, showAddDialog: false, log}
     }
     if (action.type === 'remove_log') {
-      const log = state.log.slice()
       log.splice(action.payload, 1)
-      localStorage.log = JSON.stringify(log)
       return {...state, log}
     }
     if (action.type === 'stop_log') {
-      const log = state.log.slice()
       log[action.payload].running = false
       log[action.payload].end = Date.now()
-      localStorage.log = JSON.stringify(log)
       return {...state, log}
     }
     if (action.type === 'set_weight') {
-      localStorage.weight = state.setWeight
       return {...state, showSettingsDialog: false, weight: parseFloat(state.setWeight)}
     }
     return state
   },
-  process.env.NODE_ENV === 'development' && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  defaultState,
+  composeEnhancers(
+    autoRehydrate()
+  )
 )
 
+const categories = Object.values(mets)
+  .map(m => m.category)
+  .filter((v, i, cats) => cats.indexOf(v) === i)
+store.dispatch({type: 'set', payload: {mets, categories}})
+
 setInterval(() => store.dispatch({type: 'set', payload: {currentTime: Date.now()}}), 1000)
+
+persistStore(store, {blacklist: ['mets', 'categories', 'currentTime']})
 
 export default store
