a092fa9f2ce76fba014fe2e31c188d3af6911a44
David Konsumer <konsumer@jetboystudio.com>
2017-08-27 18:36:35 -0700

    service-worker requires https

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import { Provider } from 'react-redux'
 
 import App from './App'
 import store from './store'
-import registerServiceWorker from './registerServiceWorker'
 import './index.css'
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'))
-registerServiceWorker()
